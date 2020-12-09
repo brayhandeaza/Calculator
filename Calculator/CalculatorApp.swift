5dd12793109b5859ea62322f3d10254efe3ebb13
Brayhan De Aza <brayhandeaza@gmail.com>
2020-12-09 16:06:22 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Brayhan De Aza on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
