5144dd3586782dcb28568b9d4b3c4c93e059ed04
Brayhan De Aza <brayhandeaza@gmail.com>
2020-12-09 18:26:47 -0500

    design done

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -7,15 +7,113 @@
 
 import SwiftUI
 
+enum CalculatorButtons: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide
+    case dot
+    case ac, plusMinus, percent
+    
+    var title: String {
+        switch self {
+        case .zero:
+            return "0"
+        case .one:
+            return "1"
+        case .two:
+            return "2"
+        case .three:
+            return "3"
+        case .four:
+            return "4"
+        case .five:
+            return "5"
+        case .six:
+            return "6"
+        case .seven:
+            return "7"
+        case .eight:
+            return "8"
+        case .nine:
+            return "9"
+        case .equals:
+            return "="
+        case .plus:
+            return "+"
+        case .multiply:
+            return "X"
+        case .minus:
+            return "-"
+        case .dot:
+            return "."
+        default:
+            return "AC"
+        }
+    }
+    
+    var backgoundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            return Color(.darkGray)
+        case .ac, .plusMinus, .percent:
+            return Color(.lightGray)
+        default:
+            return Color(.orange)
+        }
+    }
+}
+
+class GlobalEnvironment: ObservableObject {
+    @Published var display = "0"
+    
+    func receiveInput(button: CalculatorButtons) {
+        self.display = button.title
+    }
+    
+}
+
 struct ContentView: View {
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    let buttons: [[CalculatorButtons]] = [
+        [.ac, .plusMinus, .percent, .divide],
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .dot, .equals]
+    ]
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 12) {
+                HStack {
+                    Spacer()
+                    Text(env.display)
+                        .font(.system(size: 64))
+                        .foregroundColor(Color.white)
+                }
+                .padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            CalculatorButtonsView(button: button)
+                        }
+                    }
+                }
+                
+            }
+            .padding(.bottom)
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(GlobalEnvironment())
     }
 }
---
+++ Calculator/Views/CalculatorButtonsView.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorButtonsView.swift
+//  Calculator
+//
+//  Created by Brayhan De Aza on 12/9/20.
+//
+
+import SwiftUI
+
+struct CalculatorButtonsView: View {
+    var button: CalculatorButtons
+    @EnvironmentObject var env: GlobalEnvironment
+    
+    var body: some View {
+        Button(action: {self.env.receiveInput(button: button)}) {
+            Text(button.title)
+                .font(.system(size: 32))
+                .frame(width: self.buttonsWidth(button: button), height: (UIScreen.main.bounds.width - 5 * 12) / 4)
+                .foregroundColor(Color.white)
+                .background(button.backgoundColor)
+                .cornerRadius(self.buttonsWidth(button: button))
+        }
+    }
+    func buttonsWidth(button: CalculatorButtons) -> CGFloat {
+        if button == .zero {
+            return (UIScreen.main.bounds.width - 4 * 12) / 4 * 2
+        } else {
+            return (UIScreen.main.bounds.width - 5 * 12) / 4
+        }
+    }
+}
